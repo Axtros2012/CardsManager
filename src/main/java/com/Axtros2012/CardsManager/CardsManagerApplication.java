4fd5009f8390db254ad9c8d636143c752232030e
Axtros <axtros2012@gmail.com>
2021-12-20 20:16:55 +0100

    new: proyect creation

---
+++ src/main/java/com/Axtros2012/CardsManager/CardsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.Axtros2012.CardsManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsManagerApplication.class, args);
+	}
+
+}
